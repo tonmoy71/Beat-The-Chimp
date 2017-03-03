93dee24793326426ed82d8ee450b356e7bbff0d1
Fahim Masud Choudhury <fahim.choudhury@sekai-lab-bd.com>
2017-03-03 11:04:27 +0600

    Remove ActionBar(ToolBar) and title.

--- app/src/main/java/com/fahad/beatthechimp/activity/StarterActivity.java
+++ app/src/main/java/com/fahad/beatthechimp/activity/StarterActivity.java
@@ -1,10 +1,7 @@
 package com.fahad.beatthechimp.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
-
+import android.support.v7.app.AppCompatActivity;
 import com.fahad.beatthechimp.R;
 
 public class StarterActivity extends AppCompatActivity {
@@ -12,7 +9,6 @@ public class StarterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.setContentView(R.layout.activity_starter);
     }
 }
